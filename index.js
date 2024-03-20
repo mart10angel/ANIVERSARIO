0d32ac0a30ceb1b9ca8d4d184c46cc9b24e314f9
mart10angel <163224416+mart10angel@users.noreply.github.com>
2024-03-19 19:50:19 -0600

    mejora audio

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-<script>
-  document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById("miAudio").play()
-  });
-</script>
+document.addEventListener('DOMContentLoaded', function() {
+  var audio = document.getElementById('miAudio');
+  if (audio !== null) {
+    audio.play();
+  }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var audio = document.getElementById('miAudio');
+  if (audio !== null) {
+    // Verificar si hay un estado de reproducción guardado en el almacenamiento local
+    if (localStorage.getItem('audioPlaybackPosition')) {
+      // Obtener la posición de reproducción guardada
+      var playbackPosition = parseFloat(localStorage.getItem('audioPlaybackPosition'));
+      // Configurar la posición de reproducción del audio
+      audio.currentTime = playbackPosition;
+    }
+    // Reproducir el audio
+    audio.play();
+    
+    // Guardar el estado de reproducción actual en el almacenamiento local al pausar el audio
+    audio.addEventListener('pause', function() {
+      localStorage.setItem('audioPlaybackPosition', audio.currentTime);
+    });
+  }
+});
